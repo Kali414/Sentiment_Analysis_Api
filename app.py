7677ecc431fb0d162439b9f19b5b61d7c965b63c
Kalicharan <kalisahoo2004@gmail.com>
2025-02-23 14:00:18 +0530

    Changes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from flask_cors import CORS
 
 CORS(app, resources={
     r"/sentiment": {
-        "origins": ["http://127.0.0.1:5500", "http://localhost:5500"],
+        "origins": "*",
         "methods": ["POST"],
         "allow_headers": ["Content-Type"]
     }
